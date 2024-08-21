59ec390b5383bd28b4090999455ab07b517767ab
kingarturvitorio <kingarturvitorio@gmail.com>
2024-08-21 00:12:29 -0400

    .

--- app/urls.py
+++ app/urls.py
@@ -30,3 +30,6 @@ urlpatterns = [
     path('', include('relatorios.urls')),
     path('', include('calendario.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # Add this line
