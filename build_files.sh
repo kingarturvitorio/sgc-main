d6a0ce3e00410572c1ea1f710503ef10c1d8650c
kingarturvitorio <kingarturvitorio@gmail.com>
2024-08-21 00:44:23 -0400

    .

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
-python3.11.9 -m venv venv
-source venv/bin/activate  # ou "venv\Scripts\activate" no Windows
+python3.9 -m venv venv
+venv\Scripts\activate # ou "venv\Scripts\activate" no Windows
 pip install -r requirements.txt
 pip3 install -r requirements.txt 
 
-python3.11.9 manage.py collectstatic
+python3.9 manage.py collectstatic
